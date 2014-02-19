e8fc10feb57a7255c224c34e4bc8fc3675c1ff9b
Gökhan Sarıkan <gsarikan45@gmail.com>
2014-02-19 21:12:37 +0200

    Create faktoriyel.c

---
+++ faktoriyel.c
@@ -0,0 +1,21 @@
+long faktoriyel(long N){
+     if(N==0){
+        return 1;
+        } else {
+           return N*faktoriyel(N-1);
+           }
+}
+
+int main(int argc, char *argv[])
+{
+  
+  long sayi;
+  
+  printf("faktoriyelini hesaplamak istediginiz sayiyi giriniz: \n");
+  scanf("%ld",&sayi);
+  
+  printf("%ld!= %ld\n",sayi,faktoriyel(sayi));
+  
+  system("PAUSE");	
+  return 0;
+}
